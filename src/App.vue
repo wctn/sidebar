6e7666e944089e660ef73ca50a7c8a89c129e52e
Wichittranut  Boonlap <51953752+wctn@users.noreply.github.com>
2023-03-01 22:55:57 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <ejs-sidebar
+        id="default-sidebar"
+        ref="sidebar"
+        :showBackdrop="showBackdrop"
+        :closeOnDocumentClick="closeOnDocumentClick"
+      >
+        <div class="title">Sidebar content</div>
+        <div class="sub-title">Click the button to close the Sidebar.</div>
+        <div class="center-align">
+          <button
+            ejs-button
+            id="close"
+            v-on:click="closeClick"
+            class="e-btn close-btn"
+          >
+            Close Sidebar
+          </button>
+        </div>
+      </ejs-sidebar>
+      <div>
+        <div class="title">Main content</div>
+        <div class="sub-title">Click the button to open/close the Sidebar.</div>
+        <div style="padding: 20px" class="center-align">
+          <button
+            ejs-button
+            id="toggle"
+            class="e-btn e-info"
+            v-on:click="toggleClick"
+          >
+            Toggle Sidebar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { SidebarComponent } from "@syncfusion/ej2-vue-navigations";
+import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+import "../node_modules/@syncfusion/ej2-vue-navigations/styles/material.css";
+
+export default {
+  name: "CssClass",
+  components: {
+    "ejs-sidebar": SidebarComponent,
+  },
+  data() {
+    return {
+      animate: false,
+      enableRtl: true,
+      width: "280px",
+      type: "Push",
+    };
+  },
+  methods: {
+    toggleClick: function () {
+      this.$refs.sidebar.toggle();
+    },
+    closeClick: function () {
+      this.$refs.sidebar.hide();
+    },
+  },
+};
+</script>
+
+<style>
+.center-align {
+  text-align: center;
+  padding: 20px;
+}
+
+.title {
+  text-align: center;
+  font-size: 20px;
+  padding: 15px;
+}
+#hamburger::before {
+  content: "\e99a";
+  float: left;
+  padding-top: 20px;
+  padding-left: 10px;
+  font-size: 25px;
+}
+.sub-title {
+  text-align: center;
+  font-size: 16px;
+  padding: 10px;
+}
+
+.center {
+  text-align: center;
+  display: none;
+  font-size: 13px;
+  font-weight: 400;
+  margin-top: 20px;
+}
+
+#default-sidebar {
+  background-color: rgb(25, 118, 210);
+  color: #ffffff;
+}
+
+.close-btn:hover {
+  color: #fafafa;
+}
+</style>
